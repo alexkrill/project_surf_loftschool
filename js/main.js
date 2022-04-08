f9933a8bbca1deaa75d970f0e0529088d627b5cf
Alexey Lamkin <98019904+alexkrill@users.noreply.github.com>
2022-04-08 15:17:29 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -11291,7 +11291,7 @@ document.getElementById("closeButton").onclick = function() {
     document.getElementById("fullscreenMenu").style.display = "none";
 }
 	
-document.getElementsByClassName("vertical-menu__link").onclick = function() {
+document.getElementsByClassName("vertical-menu__item").onclick = function() {
 	document.getElementById("fullscreenMenu").style.display = "none";
 }
 
